3b4289442af1762432b39d51c2d0ce42a9844a91
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-29 23:02:58 +0530

    [Akash]Added: validate first name

--- src/main/java/com/bridgelabz/UserRegistrationTest/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistrationTest/UserRegistration.java
@@ -1,13 +1,22 @@
 package com.bridgelabz.UserRegistrationTest;
 
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
  */
 public class UserRegistration 
 {
+	private static final String FIRST_NAME_PATTERN="^[A-Z][a-z]{2,}";
+	
     public void printWelcome()
     {
         System.out.println( "Welcome to User Registration Problem with JUnit Testing" );
+    }
+    
+    public boolean checkFirstName(String fName) {
+    	Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+    	return pattern.matcher(fName).matches();
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest/UserRegistrationTest.java
@@ -2,11 +2,33 @@ package com.bridgelabz.UserRegistrationTest;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple UserRegistration.
  */
 public class UserRegistrationTest {
+	
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		UserRegistration userRegistration =  new UserRegistration();
+		boolean result = userRegistration.checkFirstName("Akash");
+		Assert.assertEquals(true,result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenWithSpChars_ShouldReturnFalse() {
+		UserRegistration userRegistration =  new UserRegistration();
+		boolean result = userRegistration.checkFirstName("Ak@sh");
+		Assert.assertEquals(false,result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenShort_ShouldReturnFalse() {
+		UserRegistration userRegistration =  new UserRegistration();
+		boolean result = userRegistration.checkFirstName("Ak");
+		Assert.assertEquals(false,result);
+	}
 	
 }
