15f243c1d958237712abed856a1d20b4199071c1
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-29 22:39:33 +0530

    [Akash]Added: welcome message

---
+++ src/main/java/com/bridgelabz/UserRegistrationTest/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.UserRegistrationTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserRegistration 
+{
+    public void printWelcome()
+    {
+        System.out.println( "Welcome to User Registration Problem with JUnit Testing" );
+    }
+}
---
+++ src/test/java/com/bridgelabz/UserRegistrationTest/UserRegistrationTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.UserRegistrationTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple UserRegistration.
+ */
+public class UserRegistrationTest {
+	
+}
